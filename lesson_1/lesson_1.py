548215ed87da22f5d5ad0ffaccaa539a18f62d9a
Leonardo <leorsantos2003@gmail.com>
2025-09-08 20:56:11 -0300

    lesson 1

--- lesson_1/lesson_1.py
+++ lesson_1/lesson_1.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 # Terminal: cd .\lesson_1\
 
@@ -25,4 +28,31 @@ model = keras.Sequential([
 
 # The weights of the neural network
 w, b = model.weights
-print("\nWeights\n{}\n\nBias\n{}".format(w, b))
+print("\nWeights\n{}\n\nBias\n{}\n".format(w, b))
+
+# --- GRAPHIC ---
+
+# Simple Model with 1 entry for graphic
+model_simple = keras.Sequential([
+    layers.Dense(1, input_shape=[1]),
+])
+
+x = tf.linspace(-1.0, 1.0, 100)
+y = model_simple.predict(x)
+
+plt.figure(dpi=100)
+plt.plot(x, y, 'k')
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
+plt.xlabel("Input: x")
+plt.ylabel("Target y")
+
+w_simple, b_simple = model_simple.weights
+plt.title("Weight: {:0.2f}\nBias: {:0.2f}".format(w_simple.numpy()[0][0], b_simple.numpy()[0]))
+
+# --- Save graphic as png file ---
+print("\nSaving gaphic as 'grafico_reta_neuronio.png'...")
+plt.savefig('linear_neuron_graphic.png')
+print("Graphic succesfully saved!\n")
+
+#plt.show()
