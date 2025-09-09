1e99e1878d06069c677a9d5c72aec176386c666f
Leonardo <leorsantos2003@gmail.com>
2025-09-09 16:03:36 -0300

    lesson 5 graphics

--- lesson_5/lesson_5.py
+++ lesson_5/lesson_5.py
@@ -52,3 +52,160 @@ input_shape = [X_train.shape[1]]
 print("Input shape: {}".format(input_shape))
 
 # --- ---
+
+# Adding two 30% dropout layers, one after 128 and one after 64
+model = keras.Sequential([
+    layers.Dense(128, activation='relu', input_shape=input_shape),
+    layers.Dropout(0.3),
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.3),
+    layers.Dense(1)
+])
+
+model.compile(
+    optimizer='adam',
+    loss='mae',
+)
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=512,
+    epochs=50,
+    verbose=0,
+)
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot()
+# print("Minimum Validation Loss: {:0.4f}".format(history_df['val_loss'].min()))
+
+# --- GRAPHIC 1 ---
+
+history_df = pd.DataFrame(history.history)
+plt.figure(figsize=(10, 6))
+plt.plot(history_df['loss'], label='Training Loss')
+plt.plot(history_df['val_loss'], label='Validation Loss')
+
+# Define os limites do eixo Y para focar na faixa de loss entre 0.15 e 0.25
+plt.ylim(0.188, 0.2)
+# Opcional: Define os limites do eixo X para ver apenas da época 10 em diante
+plt.xlim(5, 50)
+
+plt.title('Comparison between Training Loss and Validation Loss')
+plt.ylabel('Loss (MAE)')
+plt.xlabel('Epoch')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.savefig('loss_vs_val_loss_1.png')
+print("Graphic succesfully saved as 'loss_vs_val_loss_1.png'")
+# plt.show()
+
+# --- Concrete Dataset ---
+
+concrete = pd.read_csv('../concrete.csv')
+df = concrete.copy()
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+X_train = df_train.drop('CompressiveStrength', axis=1)
+X_valid = df_valid.drop('CompressiveStrength', axis=1)
+y_train = df_train['CompressiveStrength']
+y_valid = df_valid['CompressiveStrength']
+
+input_shape = [X_train.shape[1]]
+
+model = keras.Sequential([
+    layers.Dense(512, activation='relu', input_shape=input_shape),
+    layers.Dense(512, activation='relu'),    
+    layers.Dense(512, activation='relu'),
+    layers.Dense(1),
+])
+model.compile(
+    optimizer='sgd', # SGD is more sensitive to differences of scale
+    loss='mae',
+    metrics=['mae'],
+)
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=64,
+    epochs=100,
+    verbose=0,
+)
+
+history_df = pd.DataFrame(history.history)
+# history_df.loc[0:, ['loss', 'val_loss']].plot()
+print(("Minimum Validation Loss: {:0.4f}").format(history_df['val_loss'].min()))
+
+# --- GRAPHIC 2 ---
+
+history_df = pd.DataFrame(history.history)
+plt.figure(figsize=(10, 6))
+plt.plot(history_df['loss'], label='Training Loss')
+plt.plot(history_df['val_loss'], label='Validation Loss')
+
+# Define os limites do eixo Y para focar na faixa de loss entre 0.15 e 0.25
+plt.ylim(0.188, 0.2)
+# Opcional: Define os limites do eixo X para ver apenas da época 10 em diante
+plt.xlim(5, 50)
+
+plt.title('Comparison between Training Loss and Validation Loss')
+plt.ylabel('Loss (MAE)')
+plt.xlabel('Epoch')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.savefig('loss_vs_val_loss_2.png')
+print("Graphic succesfully saved as 'loss_vs_val_loss_2.png'")
+# plt.show()
+
+# --- ---
+
+# Adding four Batch Normalizion layers
+model = keras.Sequential([
+    layers.BatchNormalization(input_shape=input_shape),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(512, activation='relu'),
+    layers.BatchNormalization(),
+    layers.Dense(1),
+])
+
+model.compile(
+    optimizer='sgd',
+    loss='mae',
+    metrics=['mae'],
+)
+EPOCHS = 100
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_valid, y_valid),
+    batch_size=64,
+    epochs=EPOCHS,
+    verbose=0,
+)
+
+history_df = pd.DataFrame(history.history)
+# history_df.loc[0:, ['loss', 'val_loss']].plot()
+print(("Minimum Validation Loss: {:0.4f}").format(history_df['val_loss'].min()))
+
+# --- GRAPHIC 3 ---
+
+history_df = pd.DataFrame(history.history)
+plt.figure(figsize=(10, 6))
+plt.plot(history_df['loss'], label='Training Loss')
+plt.plot(history_df['val_loss'], label='Validation Loss')
+
+# Define os limites do eixo Y para focar na faixa de loss entre 0.15 e 0.25
+plt.ylim(0, 40)
+# Opcional: Define os limites do eixo X para ver apenas da época 10 em diante
+plt.xlim(0, 100)
+
+plt.title('Comparison between Training Loss and Validation Loss')
+plt.ylabel('Loss (MAE)')
+plt.xlabel('Epoch')
+plt.legend()
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.savefig('loss_vs_val_loss_3.png')
+print("Graphic succesfully saved as 'loss_vs_val_loss_3.png'")
+# plt.show()
